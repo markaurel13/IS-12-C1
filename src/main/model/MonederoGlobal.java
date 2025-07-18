0837b39c622c9df53b2b385c5efde8ea71bb0b23
Barbie RM <barbara.1297@gmail.com>
2025-07-17 21:12:41 -0400

    Creación del monedero global (complementario al monedero virtual, cada vez que haya una recarga o pago de menú cambiarán los montos del monedero global). Creado para tener la distinción entre el monedero de usuario y el de aplicación en sí. Uso del patrón Singleton.

---
+++ src/main/model/MonederoGlobal.java
@@ -0,0 +1,28 @@
+package main.model;
+
+import java.text.DecimalFormat;
+
+// creada con patron Singleton (un solo monedero global)
+public class MonederoGlobal {
+    private double saldoTotal;
+    private double saldoPagado;
+    private static MonederoGlobal instancia = null;
+
+    private MonederoGlobal() { // Constructor privado
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        this.saldoTotal = 0;
+        this.saldoPagado = 0;
+    }
+
+    public static getInstancia() {
+        if (MonederoGlobal.instancia == null){
+            MonederoGlobal.instancia = new MonederoGlobal();
+        }
+        return MonederoGlobal.instancia;
+    }
+
+}
