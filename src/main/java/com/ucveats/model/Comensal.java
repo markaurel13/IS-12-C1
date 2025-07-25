79884c7d0c4db17b3d8c852c0189cc619228405c
Barbie RM <barbara.1297@gmail.com>
2025-07-25 09:04:43 -0400

    Return comensal previo

--- src/main/java/com/ucveats/model/Comensal.java
+++ src/main/java/com/ucveats/model/Comensal.java
@@ -6,8 +6,7 @@ package com.ucveats.model;
  * Extiende de {@link Usuario} con rol predefinido COMENSAL.
  */
 public class Comensal extends Usuario {
-    private MonederoVirtual Monedero;
-    private RolComensal rol;
+    MonederoVirtual Monedero;
     /**
      * Crea una instancia de Comensal.
      * @param cedula Cédula del comensal.
@@ -16,38 +15,9 @@ public class Comensal extends Usuario {
      * @param passwordHash Contraseña hasheada.
      * @throws IllegalArgumentException Si algún dato es inválido.
      */
-    public Comensal(String cedula, String correo, String telefono, String passwordHash, RolComensal rol) {
+    public Comensal(String cedula, String correo, String telefono, String passwordHash) {
         super(cedula, correo, telefono, passwordHash, Usuario.RolUsuario.COMENSAL);
         // Creacion del monedero virtual asignado a este usuario
-        this.Monedero = MonederoVirtual.MonederoVirtual();
-
-        // Validación de rol:
-        if (rol == null) {
-            throw new IllegalArgumentException("El rol no puede ser nulo");
-        }
-
-        this.rol = rol;
+        Monedero = MonederoVirtual.MonederoVirtual(); 
     }
-
-    // --- Setter para el rol ---
-    public void setRol(RolComensal rol) {
-
-        if (rol == null) {
-            throw new IllegalArgumentException("El rol no puede ser nulo");
-        }
-
-        this.rol = rol;
-    }
-
-    // Enumeración de roles posibles:
-    public enum RolComensal {
-        ESTUDIANTE, PROFESOR, EMPLEADO;
-
-        // Versión "bonita" del nombre del rol:
-        @Override
-        public String toString() {
-            return name().charAt(0) + name().substring(1).toLowerCase();
-        }
-    }
-
 }
